701e974f4f2c2b8c6c38a6cbf861e0fab9dac79f
Peter Nagy <petnagy@gmail.com>
2018-07-25 00:07:02 +0200

    Add landmark detection

--- app/src/main/java/com/playground/firebase/ml/playground/RemoteMlActivity.kt
+++ app/src/main/java/com/playground/firebase/ml/playground/RemoteMlActivity.kt
@@ -5,17 +5,26 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.base_layout.*
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.app.Activity
+import android.content.Intent
 import android.support.v4.app.ActivityCompat
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.support.v4.content.ContextCompat
 import android.view.View
+import com.google.android.gms.tasks.Task
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.cloud.FirebaseVisionCloudDetectorOptions
+import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmark
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import timber.log.Timber
 
 
 class RemoteMlActivity : AppCompatActivity() {
 
     companion object {
-        const val CAMERA_REQUEST_CODE = 1234
+        const val CAMERA_PERMisSION_REQUEST_CODE = 1234
+        const val CAMERA_REQUEST_CODE = 34242
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +37,7 @@ class RemoteMlActivity : AppCompatActivity() {
 
     private fun checkAndLaunchCamera() {
         if (ContextCompat.checkSelfPermission(this, CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(CAMERA, WRITE_EXTERNAL_STORAGE), CAMERA_REQUEST_CODE)
+            ActivityCompat.requestPermissions(this, arrayOf(CAMERA, WRITE_EXTERNAL_STORAGE), CAMERA_PERMisSION_REQUEST_CODE)
         } else {
             launchCamera()
         }
@@ -37,7 +46,7 @@ class RemoteMlActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (requestCode == CAMERA_REQUEST_CODE) {
+        if (requestCode == CAMERA_PERMisSION_REQUEST_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
                     && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                 launchCamera()
@@ -45,8 +54,38 @@ class RemoteMlActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            var resultText = ""
+            val photo = data?.extras?.get("data") as Bitmap
+            image.setImageBitmap(photo)
+            val options = FirebaseVisionCloudDetectorOptions.Builder()
+                    .setModelType(FirebaseVisionCloudDetectorOptions.LATEST_MODEL)
+                    .setMaxResults(15)
+                    .build()
+            val image = FirebaseVisionImage.fromBitmap(photo)
+            val detector = FirebaseVision.getInstance().getVisionCloudLandmarkDetector(options)
+            val result: Task<List<FirebaseVisionCloudLandmark>> = detector.detectInImage(image).addOnSuccessListener { list ->
+                Timber.d("SUCCESS")
+                Timber.d("size of list: %s", list.size)
+                resultText += "Landmark detection: "
+                if (list.isNotEmpty()) {
+                    list.map { landmark ->
+                        resultText += "[${landmark.landmark}] "
+                    }
+                }
+                response.text = resultText
+            }.addOnFailureListener{
+                Timber.e("FAILED %s", it.localizedMessage)
+            }
+        }
+    }
+
     private fun launchCamera() {
         Timber.d("Launch Camera")
+        startActivityForResult(Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE), CAMERA_REQUEST_CODE)
+        response.text = ""
     }
 
 }
